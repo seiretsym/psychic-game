152e0d3c4d5ab8b77dbf34d92fb96ee37b860d2c
Kerwin Hy <kerwin.hy@gmail.com>
2019-08-29 14:47:57 -0700

    add alphabet array and empty functions

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -7,7 +7,32 @@ var textWins = document.getElementById("wins"),
     textGuesses = document.getElementById("guesses"),
     textGuessed = document.getElementById("guessed");
 
+// alphabet array
+var alpha = ["abcdefghijklmnopqrstuvwxyz"];
+
 /// make functions
 
+// add point to win and reset game
+function win() {
+
+}
+
+// add point to lose and reset game
+function lose() {
+
+}
+
+// select a random letter
+function randChar() {
+
+}
+
+// restart game 
+function reGame() {
+
+}
 
 /// on key press events
+document.onkeyup = function(event) {
+
+}
