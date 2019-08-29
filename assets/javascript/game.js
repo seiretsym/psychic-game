d227f942edfcafdd8cb7e445618cb6b7d7c3b47f
Kerwin Hy <kerwin.hy@gmail.com>
2019-08-29 14:44:00 -0700

    declare text variables in game.js

---
+++ assets/javascript/game.js
@@ -0,0 +1,13 @@
+//// Begin Psychic Game Scripts
+
+/// declare variables
+// text variables
+var textWins = document.getElementById("wins"),
+    textLosses = document.getElementById("losses"),
+    textGuesses = document.getElementById("guesses"),
+    textGuessed = document.getElementById("guessed");
+
+/// make functions
+
+
+/// on key press events
